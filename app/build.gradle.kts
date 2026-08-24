d37bc45c4b2d48bf29251d45f5016556d8b557a5
Stevensss <katachishoya382@gmail.com>
2026-08-24 20:28:59 +0800

    fix: Android Sdk version in build.gradle.kts to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.alera.payloadextraction"
         minSdk = 30
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
